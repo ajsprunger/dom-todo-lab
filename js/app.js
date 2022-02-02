1a303d70a335845d54c922caab7f4be5a909e925
austinjs <ajsprunger@gmail.com>
2022-02-02 15:58:47 -0700

    adds delete item on click

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
 const userInput = document.getElementById('user-input')
 const btn = document.getElementById('submit-button')
 const list = document.getElementById('todo-list')
+const reset = document.getElementById('reset')
 
 btn.addEventListener('click', function(evt){
   const newLi = document.createElement('li')
   newLi.innerText = userInput.value
-  if (userInput !== ''){
+  if (userInput.value !== ''){
     list.appendChild(newLi)
     userInput.value = ''
   }
 })
 
+reset.addEventListener('click', function(evt){
+  userInput.value = ''
+  const wholeList = document.querySelectorAll('li')
+  list.remove(wholeList)
+})
+
+
+document.querySelector('ul').addEventListener('click', handleClick)
+
+function handleClick(evt){
+  evt.target.parentNode.removeChild(evt.target)
+}
 
 
